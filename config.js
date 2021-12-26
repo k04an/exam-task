ec17928c8be21c1101dc3158e564f744b8e99196
k04an <ne.k04an@ya.ru>
2021-12-27 01:07:49 +0700

    Изменены данные для подключения к бд 2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     DATABASE_HOST: 'db4free.net',
-    DATABASE_NAME: 'examtaskbd',
+    DATABASE_NAME: 'examtaskdb',
     DATABASE_USER: 'nek04an',
     DATABASE_PASSWORD: 'qwerty123',
     API_PREFIX: '/api/',
